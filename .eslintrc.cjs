bbb4164077181cb0c87119ee28ba2e11101bbd9d
Kennedy Chin <zxc1258741@gmail.com>
2022-10-25 23:29:24 +0800

    initialize vue careers

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  env: {
+    node: true,
+  },
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
