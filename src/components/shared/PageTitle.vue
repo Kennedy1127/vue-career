cdaa98be081aca08cae74cbc2b40a5ed233a0083
Kennedy Chin <zxc1258741@gmail.com>
2022-11-14 20:25:21 +0800

    add PageTitle to Teams

---
+++ src/components/shared/PageTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="border-b border-solid border-brand-grey-1">
+    <div class="my-16 px-4">
+      <div class="flex flex-col items-center home-container mx-auto">
+        <h1
+          class="home-md:text-[40px] home-sm:text-[32px] text-[30px] text-input-text-grey"
+        >
+          {{ props.title }}
+        </h1>
+        <div class="max-w-[650px]">
+          <p
+            class="home-sm:text-base text-sm text-text-grey font-light text-center my-4"
+          >
+            {{ props.text }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>Teams</div>
+  <PageTitle
+    title="Teams"
+    text="Together, we create access to information and build products that
+            improve people's lives. Want to be a Googler? Find your role."
+  />
 </template>
+
+<script setup>
+import PageTitle from '@/components/shared/PageTitle.vue';
+</script>
